62394cb75a3dbe5939697bdaa475803b5d71a01b
Leandro Farias <lwcfarias@gmail.com>
2022-05-08 11:13:54 +0200

    initial arch design

---
+++ src/main/java/com/superchat/messaging/SuperchatBackendChallengeLeandroApplication.java
@@ -0,0 +1,13 @@
+package com.superchat.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperchatBackendChallengeLeandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperchatBackendChallengeLeandroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superchat/messaging/SuperchatBackendChallengeLeandroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superchat.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperchatBackendChallengeLeandroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
